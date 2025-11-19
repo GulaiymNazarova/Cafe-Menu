0550853a950956b00d89e0a5e47a7f41b5190007
Nazarova Gulaiym <gulaiym.nazarova@alatoo.edu.kg>
2025-11-19 16:50:26 +0600

    Добавили Menu Service

---
+++ src/main/java/com/example/demo/service/MenuService.java
@@ -0,0 +1,45 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Category;
+import com.example.demo.model.Item;
+import com.example.demo.repository.CategoryRepository;
+import com.example.demo.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuService {
+
+    private final CategoryRepository categoryRepository;
+    private final ItemRepository itemRepository;
+
+    public MenuService(CategoryRepository categoryRepository, ItemRepository itemRepository) {
+        this.categoryRepository = categoryRepository;
+        this.itemRepository = itemRepository;
+    }
+
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void deleteCategory(Integer id) {
+        categoryRepository.deleteById(id);
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Item saveItem(Item item) {
+        return itemRepository.save(item);
+    }
+
+    public void deleteItem(Integer id) {
+        itemRepository.deleteById(id);
+    }
+
+    public List<Item> getItemsByCategory(Integer categoryId) {
+        return itemRepository.findByCategoryId(categoryId);
+    }
+}
